a90ead42a5c1cef6756f6ea39929d43a6f932ae4
afroz23 <afroziiitian@gmail.com>
2022-02-16 10:16:01 +0530

    ready to host on github pages

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Meme generator
 
-- It fetches data from [Meme Api](https://api.imgflip.com/get_memes).
-- After pick a template you will proceed to a from where you need to give top text and bottom text.
-- Responsive
-- [Live](https://afroz23.github.io/growth_school/)
+-   It fetches data from [Meme Api](https://api.imgflip.com/get_memes).
+-   After pick a template you will proceed to a from where you need to give top text and bottom text.
+-   Responsive
+-   [Live](https://afroz23.github.io/meme_generator/)
--- src/Component/Fetchdata.js
+++ src/Component/Fetchdata.js
@@ -2,114 +2,123 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const objectToQueryParam = (obj) => {
-  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
-  return "?" + params.join("&");
+    const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+    return "?" + params.join("&");
 };
 export default function Fetchdata() {
-  const [Memes, setMemes] = useState([]);
-  const [Meme, setMeme] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [Text, setText] = useState({ TopText: "", BottomText: "" });
-  const [header, setHeader] = useState("Pick a Template");
-  const [meme, setmeme] = useState(null);
-  const [Error, setError] = useState("");
+    const [Memes, setMemes] = useState([]);
+    const [Meme, setMeme] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [Text, setText] = useState({ TopText: "", BottomText: "" });
+    const [header, setHeader] = useState("Pick a Template");
+    const [meme, setmeme] = useState(null);
+    const [Error, setError] = useState("");
 
-  const url = "https://api.imgflip.com/get_memes";
+    const url = "https://api.imgflip.com/get_memes";
 
-  useEffect(() => {
-    axios.get(url).then((res) => {
-      setMemes(res.data.data.memes);
-      setLoading(!loading);
-    });
-  }, []);
+    useEffect(() => {
+        axios.get(url).then((res) => {
+            setMemes(res.data.data.memes);
+            setLoading(!loading);
+        });
+    }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (Text.TopText === "" || Text.BottomText === "") {
-      return setError("Invalid input");
-    }
-    setText({ TopText: "", BottomText: "" });
-    const params = {
-      template_id: Meme.id,
-      text0: Text.TopText,
-      text1: Text.BottomText,
-      username: "afroz23",
-      password: "dhu3lpdeyv",
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (Text.TopText === "" || Text.BottomText === "") {
+            return setError("Invalid input");
+        }
+        setText({ TopText: "", BottomText: "" });
+        const params = {
+            template_id: Meme.id,
+            text0: Text.TopText,
+            text1: Text.BottomText,
+            username: "afroz23",
+            password: "dhu3lpdeyv",
+        };
+        axios
+            .post(
+                `https://api.imgflip.com/caption_image${objectToQueryParam(
+                    params
+                )}`
+            )
+            .then((response) => {
+                console.log(response.data.data);
+                setmeme(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
-    axios
-      .post(
-        `https://api.imgflip.com/caption_image${objectToQueryParam(params)}`
-      )
-      .then((response) => {
-        console.log(response.data.data);
-        setmeme(response.data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  if (meme) {
-    return (
-      <div className="meme-generated">
-        <img src={meme.url} alt="meme" />
-        <div className="social">
-          <p>LinkedIn</p>
-          <p>GitHub</p>
-          <p>Facebook</p>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div className="App">
-      <h1 className="heading">{header}</h1>
-
-      <div>{loading && <h1>loading...</h1>}</div>
-      {Meme && (
-        <form onSubmit={handleSubmit} className="meme-generator-container">
-          <div className="meme-alignment">
-            <div>
-              <img src={Meme.url} alt="picked" className="meme" />
+    if (meme) {
+        return (
+            <div className="meme-generated">
+                <img src={meme.url} alt="meme" />
+                <div className="social">
+                    <p>LinkedIn</p>
+                    <p>GitHub</p>
+                    <p>Facebook</p>
+                </div>
             </div>
-          </div>
-          <div className="input-field">
-            <p className="error">{Error}</p>
-            <input
-              type="text"
-              value={Text.TopText}
-              placeholder="top text"
-              onChange={(e) => setText({ ...Text, TopText: e.target.value })}
-            />
-            <input
-              type="text"
-              value={Text.BottomText}
-              placeholder="botton text"
-              onChange={(e) => setText({ ...Text, BottomText: e.target.value })}
-            />
-            <button type="submit" className="generate-meme">
-              Generate meme
-            </button>
-          </div>
-        </form>
-      )}
-      <div>
-        {!loading && !Meme && (
-          <div className="meme-container">
-            {Memes.map((meme) => (
-              <div className="child-container" key={meme.id}>
-                <img
-                  src={meme.url}
-                  alt="meme"
-                  onClick={() => {
-                    setMeme(meme);
-                    setHeader("Meme Generator");
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        );
+    }
+    return (
+        <div className="App">
+            <h1 className="heading">{header}</h1>
+
+            <div>{loading && <h1>loading...</h1>}</div>
+            {Meme && (
+                <form
+                    onSubmit={handleSubmit}
+                    className="meme-generator-container"
+                >
+                    <div className="meme-alignment">
+                        <div>
+                            <img src={Meme.url} alt="picked" className="meme" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p className="error">{Error}</p>
+                        <input
+                            type="text"
+                            value={Text.TopText}
+                            placeholder="top text"
+                            onChange={(e) =>
+                                setText({ ...Text, TopText: e.target.value })
+                            }
+                        />
+                        <input
+                            type="text"
+                            value={Text.BottomText}
+                            placeholder="botton text"
+                            onChange={(e) =>
+                                setText({ ...Text, BottomText: e.target.value })
+                            }
+                        />
+                        <button type="submit" className="generate-meme">
+                            Generate meme
+                        </button>
+                    </div>
+                </form>
+            )}
+            <div>
+                {!loading && !Meme && (
+                    <div className="meme-container">
+                        {Memes.map((meme) => (
+                            <div className="child-container" key={meme.id}>
+                                <img
+                                    src={meme.url}
+                                    alt="meme"
+                                    onClick={() => {
+                                        setMeme(meme);
+                                        setHeader("Meme Generator");
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
